fb23c8d23956b900a4e803790630732ec1af3e1a
m6ttw <matthewrhyswilliams@live.com>
2021-02-09 17:22:38 +0000

    book has title

---
+++ src/test/java/BookTest.java
@@ -0,0 +1,19 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class BookTest {
+
+    Book book;
+
+    @Before
+    public void before(){
+        book = new Book("The Hobbit", "J.R.R. Tolkien", "Fantasy");
+    }
+
+    @Test
+    public void hasTitle(){
+        assertEquals("The Hobbit", book.getTitle());
+    }
+}
